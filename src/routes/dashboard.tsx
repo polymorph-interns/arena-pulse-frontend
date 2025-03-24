63edf2baacb7eca208543a8033c1918822d8a533
Selasie5 <selasisepenu5@gmail.com>
2025-03-24 15:26:15 +0000

    feat:build out UI for sidebar

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,24 +1,89 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router'
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { 
+  SidebarProvider, 
+  SidebarTrigger,
+  Sidebar,
+  SidebarContent,
+  // SidebarHeader,
+  // SidebarFooter,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+  SidebarHeader,
+  SidebarFooter
+
+ } from '@/components/ui/sidebar'
+ import { Calendar ,Club ,Boxes} from 'lucide-react'
 
 export const Route = createFileRoute('/dashboard')({
   component: Dashboard,
 })
 
+
+const AppSidebar= ()=>
+{
+  const items = [
+    {
+      title: "Teams",
+      url: "#",
+      icon: Club,
+    },
+    {
+      title: "Leagues",
+      url: "#",
+      icon: Boxes,
+    },
+    {
+      title: "Fixtures",
+      url: "#",
+      icon: Calendar,
+    }
+  ]
+  return(
+    <Sidebar collapsible='offcanvas' className="dark text-white">
+      <SidebarHeader className='font-clash-bold text-orange-300 text-2xl'>ArenaPulse</SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu className='space-y-4'>
+            {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild  className='font-clash-regular text-[1rem] text-white hover:bg-orange-500 rounded-none'>
+                    <a href={item.url}>
+                      <item.icon size={50} />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+              </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter className='flex flex-row justify-center items-center border border-t-gray-100/30 py-5'>
+      <img src="/profile.jpg" className='w-10 h-10 rounded-full' alt=""/>
+              <div className='flex flex-col'>
+              <span className='font-satoshi-regular text-white text-sm'>Jennifer Burger</span>
+              <span className='font-satoshi-regular text-white text-sm'>engineering@polymorphlabs.io</span>
+              </div>
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
 function Dashboard() {
   return (
     <>
-    <div className='flex justify-center items-center'>
- {/* Sidebar */}
- Test
-    <div className="h-screen w-1/5 bg-white">
-
-    </div>
-    <div className='h-screen w-4/5 bg-gray-300'>
-    <Outlet/>
-      </div>
- 
-    </div>
+    <SidebarProvider>
+    <AppSidebar/>
+      <main>
+      <SidebarTrigger/>
+      {/* <Outlet /> */}
+<Outlet/>
+        </main>
+      </SidebarProvider>
    
 
   
